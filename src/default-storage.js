04830c896691065c416dbb480d0e82a81c0bceba
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-09-12 14:48:11 +0200

    refactor: remove window. prefix

--- src/default-storage.js
+++ src/default-storage.js
@@ -2,28 +2,28 @@ import { RbStorage } from "rb-core-module";
 
 export class DefaultStorage extends RbStorage {
   getItem (key) {
-    let res = window.localStorage.getItem(key)
+    let res = localStorage.getItem(key)
     if (!res) {
-      res = window.sessionStorage.getItem(key)
+      res = sessionStorage.getItem(key)
     }
     return res
   }
 
   setItem (key, val, persistent) {
     if (persistent) {
-      window.localStorage.setItem(key, val)
+      localStorage.setItem(key, val)
     } else {
-      window.sessionStorage.setItem(key, val)
+      sessionStorage.setItem(key, val)
     }
   }
 
   removeItem (key) {
-    window.localStorage.removeItem(key)
-    window.sessionStorage.removeItem(key)
+    localStorage.removeItem(key)
+    sessionStorage.removeItem(key)
   }
 
   isItemPersistent (key) {
-    const res = window.localStorage.getItem(key)
+    const res = localStorage.getItem(key)
     return !!res
   }
 }
--- test/getidentity.test.js
+++ test/getidentity.test.js
@@ -1,26 +1,9 @@
 import * as t from 'tap'
 import createAuthProvider from '../src/index'
 
-function mockStorage () {
-  const _storage = {}
-  return {
-    setItem: (key, val, persistent) => { _storage[key] = { val, persistent } },
-    getItem: (key) => _storage[key] && _storage[key].val,
-    removeItem: (key) => { delete _storage[key] },
-    isItemPersistent: (key) => _storage[key] && _storage[key].persistent,
-  }
-}
-
-function createAuthTestProvider (opts) {
-  return createAuthProvider({
-    ...opts,
-    storage: mockStorage(),
-  })
-}
-
 t.test('getIdentity', async t => {
   t.test('without passing a user', async t => {
-    const provider = createAuthTestProvider('https://my.api.url/auth')
+    const provider = createAuthProvider('https://my.api.url/auth')
     const res = await provider.getIdentity()
     t.equal(res, '', 'should return an empty string')
   })
